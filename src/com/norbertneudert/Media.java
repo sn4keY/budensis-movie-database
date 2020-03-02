148c4a022965acdee3c03c569e17407d05f2c134
Norbert Neudert <norbertneudert@gmail.com>
2020-03-02 12:08:49 +0100

    Updated Media.java with missing lists

--- src/com/norbertneudert/Media.java
+++ src/com/norbertneudert/Media.java
@@ -2,10 +2,13 @@ package com.norbertneudert;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Media {
     private BigDecimal id;
     private String title;
     private String description;
     private LocalDate premier;
+    private List<Review> reviews;
+    private List<Actor> cast;
 }

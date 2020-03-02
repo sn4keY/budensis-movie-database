e11bc5e54824b573af63ba303037654004595909
Norbert Neudert <norbertneudert@gmail.com>
2020-03-02 11:50:36 +0100

    Added Media.java class

---
+++ src/com/norbertneudert/Media.java
@@ -0,0 +1,11 @@
+package com.norbertneudert;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Media {
+    private BigDecimal id;
+    private String title;
+    private String description;
+    private LocalDate premier;
+}

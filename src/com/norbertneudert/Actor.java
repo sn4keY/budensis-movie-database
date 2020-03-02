8f4b9598bc576faa7ab993d36d4e02a4dae80793
Norbert Neudert <norbertneudert@gmail.com>
2020-03-02 11:57:27 +0100

    Added Actor.java class

---
+++ src/com/norbertneudert/Actor.java
@@ -0,0 +1,11 @@
+package com.norbertneudert;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Actor {
+    private String name;
+    private LocalDate born;
+    private String biography;
+    private List<Media> filmography;
+}

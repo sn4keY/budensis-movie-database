6f7332c0e02d86849f1d6f0cd2e1f30c27cba52c
Norbert Neudert <norbertneudert@gmail.com>
2020-03-02 12:27:43 +0100

    Created View.java class, currently with empty function bodies

---
+++ src/com/norbertneudert/bmdb/View.java
@@ -0,0 +1,21 @@
+package com.norbertneudert.bmdb;
+
+import com.norbertneudert.bmdb.domain.User;
+
+public class View {
+    public User readUserData() {
+        return new User(); // TODO: write function body that identifies the user.
+    }
+
+    public void printWelcomeMessage(User user) {
+        // TODO: write function body that prints out a welcome message based on the user.
+    }
+
+    public void printMedias() {
+        // TODO: write function body that prints out all the medias.
+    }
+
+    public void printReviews(User user) {
+        // TODO: write function body that prints out the current user's reviews.
+    }
+}

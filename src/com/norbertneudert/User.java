196c11f4faab790a0ccfe01e02fb0fcefd13ede3
Norbert Neudert <norbertneudert@gmail.com>
2020-03-02 12:10:41 +0100

    Added a list of reviews to User class

--- src/com/norbertneudert/User.java
+++ src/com/norbertneudert/User.java
@@ -1,7 +1,10 @@
 package com.norbertneudert;
 
+import java.util.List;
+
 public class User {
     private String name;
     private String email;
     private String passWord;
+    private List<Review> reviews;
 }

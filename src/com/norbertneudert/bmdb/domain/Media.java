e763379452ed49a0c5b2550d760a47a47a11ee29
Norbert Neudert <norbertneudert@gmail.com>
2020-03-09 13:15:29 +0100

    Added getters and setters to Media class

--- src/com/norbertneudert/bmdb/domain/Media.java
+++ src/com/norbertneudert/bmdb/domain/Media.java
@@ -2,6 +2,7 @@ package com.norbertneudert.bmdb.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 public class Media {
@@ -11,4 +12,57 @@ public class Media {
     private LocalDate premier;
     private List<Review> reviews;
     private List<Actor> cast;
+
+    public Media() {
+        this.reviews = Collections.emptyList();
+        this.cast = Collections.emptyList();
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getPremier() {
+        return premier;
+    }
+
+    public void setPremier(LocalDate premier) {
+        this.premier = premier;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<Actor> getCast() {
+        return cast;
+    }
+
+    public void setCast(List<Actor> cast) {
+        this.cast = cast;
+    }
 }

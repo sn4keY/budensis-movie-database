7f2dabb06993cda2793d4573bc9ae529ca32399f
Norbert Neudert <norbertneudert@gmail.com>
2020-03-09 13:15:14 +0100

    Added getters and setters to Actor class

--- src/com/norbertneudert/bmdb/domain/Actor.java
+++ src/com/norbertneudert/bmdb/domain/Actor.java
@@ -1,6 +1,7 @@
 package com.norbertneudert.bmdb.domain;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 public class Actor {
@@ -9,4 +10,48 @@ public class Actor {
     private Sex sex;
     private String biography;
     private List<Media> filmography;
+
+    public Actor() {
+        this.setFilmography(Collections.emptyList());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBorn() {
+        return born;
+    }
+
+    public void setBorn(LocalDate born) {
+        this.born = born;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public List<Media> getFilmography() {
+        return filmography;
+    }
+
+    public void setFilmography(List<Media> filmography) {
+        this.filmography = filmography;
+    }
 }

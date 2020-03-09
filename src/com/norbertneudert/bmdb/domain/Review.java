4efaa89ef43b0b39d93ad180baef2c27825eca6e
Norbert Neudert <norbertneudert@gmail.com>
2020-03-09 13:15:45 +0100

    Added getters and setters to Review class

--- src/com/norbertneudert/bmdb/domain/Review.java
+++ src/com/norbertneudert/bmdb/domain/Review.java
@@ -5,4 +5,38 @@ public class Review {
     private User creator;
     private Media reviewedMedia;
     private Rating rating;
+
+    public Review() {}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public Media getReviewedMedia() {
+        return reviewedMedia;
+    }
+
+    public void setReviewedMedia(Media reviewedMedia) {
+        this.reviewedMedia = reviewedMedia;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
 }

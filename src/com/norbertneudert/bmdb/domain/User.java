739b5fb0b652758c58ebfa6b248cce7694e5a566
Norbert Neudert <norbertneudert@gmail.com>
2020-03-09 13:16:00 +0100

    Added getters and setters to User class

--- src/com/norbertneudert/bmdb/domain/User.java
+++ src/com/norbertneudert/bmdb/domain/User.java
@@ -1,5 +1,6 @@
 package com.norbertneudert.bmdb.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class User {
@@ -7,4 +8,40 @@ public class User {
     private String email;
     private String passWord;
     private List<Review> reviews;
+
+    public User() {
+        this.setReviews(Collections.emptyList());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }

3d9fa5d80f2e9550dbbf652a7c250c9b74ea3517
Norbert Neudert <norbertneudert@gmail.com>
2020-03-09 13:18:47 +0100

    Basic implementation of the main method

--- src/com/norbertneudert/bmdb/Main.java
+++ src/com/norbertneudert/bmdb/Main.java
@@ -1,8 +1,12 @@
 package com.norbertneudert.bmdb;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        App app = new App(new Service(), new View());
 
+        app.play();
     }
 }
